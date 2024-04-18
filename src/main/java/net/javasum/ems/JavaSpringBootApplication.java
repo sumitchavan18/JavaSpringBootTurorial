9f28d634d0dd28a038ca981debf4d03cc7b34e98
Sumitc018 <sumit.chavan@cogoport.com>
2024-04-18 14:49:14 +0530

    -- naming convection correction

--- src/main/java/net/javasum/ems/JavaSpringBootApplication.java
+++ src/main/java/net/javasum/ems/JavaSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmsBackendApplication {
+public class JavaSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmsBackendApplication.class, args);
+		SpringApplication.run(JavaSpringBootApplication.class, args);
 	}
 
 }
